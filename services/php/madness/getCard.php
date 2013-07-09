0ba295e2c2ce6eb99e8f785eb7e21c6168a464d4
Max Drozd Bobak <maxim.drozd@gmail.com>
2013-07-08 23:57:35 -0400

    added a demo with card drawing

--- services/php/madness/getCard.php
+++ services/php/madness/getCard.php
@@ -2,13 +2,18 @@
     include "TCCGMySQL.class.php";
     include "localhostdb.php";
 
+    $configDeckSize = 40;
+
     $mysql = new TCCGMySQL($connection_information);
     $result = $mysql->query('SELECT COUNT(*) as count FROM cards');
     $count = $result[0][0]["count"];
-    $rowNumber = rand(0, $count-1);
 
-    $result = $mysql->query("select * from cards limit ".$rowNumber.",1");
-
+    $deck = array();
+    for($i=0;$i<$configDeckSize;$i++){
+        $rowNumber = rand(0, $count-1);
+        $result = $mysql->query("select * from cards limit ".$rowNumber.",1");
+        $deck[] = $result[0]["cards"];
+    }
     header('Content-Type: application/json');
-    echo json_encode($result[0]["cards"]);
+    echo json_encode($deck);
 ?>
