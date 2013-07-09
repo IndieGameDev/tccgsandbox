e69a1c99cf6a9c15b6824a7e86575e35c86727fa
Max Drozd Bobak <maxim.drozd@gmail.com>
2013-07-08 20:37:25 -0400

    adding new Deck and Player objects

---
+++ src/config.js
@@ -0,0 +1,11 @@
+var dojoConfig = {
+    baseUrl: "http://localhost/TCCG/tccgsandbox/",
+    tlmSiblingOfDojo: false,
+    async: true,
+    packages: [
+        { name: "dojo", location: "lib/dojo" },
+        { name: "dijit", location: "lib/dijit" },
+        { name: "dojox", location: "lib/dojox" },
+        { name: "sandbox", location: "lib/sandbox" }
+    ]
+};
